a7c40bba8218425dc1d5b1451183af96d0c2896b
vignesh <vigneshfornavy@gmail.com>
2023-12-19 10:48:46 +0530

    inital setup

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = <h2>Swiggy</h2>;
+
+const root = ReactDOM.createRoot(document.getElementById("swiggy"));
+
+root.render(heading);
